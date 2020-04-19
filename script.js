eb82229f963cdd1150b28c22dd2e07050c61c72e
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-04-20 03:30:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 // save selected movie index and price 
@@ -26,6 +28,26 @@ count.innerText = selectedSeatsCount;
 total.innerText = selectedSeatsCount * ticketPrice;
  
 }
+
+//get data from localstorage and populate UI 
+function populateUI() {
+const selectedSeats = (localStorage.getItem('selectedSeats ')
+ );
+
+if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+        if (selectedSeats.indexOf (index) > -1) {
+        
+        seat.classList.add('selected');
+    }
+    });
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectesMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
+}
+
+ 
 // movie select event 
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
@@ -42,3 +64,5 @@ updateSelectedCount();
 
     }
 });
+// initial count and total set 
+updateSelectedCount();
